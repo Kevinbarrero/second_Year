a825ce3093d56b9c17bda75ebcf7a2a0dcf10d51
holdandup <kevinbar98@gmail.com>
2021-09-12 15:49:12 +0300

    clear_files

--- Laboratory_2/Server_number.java
+++ Laboratory_2/Server_number.java
@@ -1,20 +1,15 @@
 package Laboratory_2;
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
 
 public class Server_number {
 
-    private static Socket clientSocket; //сокет для общения
-    private static ServerSocket server; // серверсокет
-    private static BufferedReader in; // поток чтения из сокета
-    private static BufferedWriter out; // поток записи в сокет
-
+    private static Socket clientSocket;
+    private static ServerSocket server;
+    private static BufferedReader in;
+    private static BufferedWriter out;
     public static void main(String[] args) throws IOException {
         try {
             server = new ServerSocket(10000);
@@ -28,10 +23,8 @@ public class Server_number {
                 out.write("Give me the number, you have: " + attempts + " attempts"+"\n");
                 out.flush();
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                String word = in.readLine();
                 System.out.println(word);
-                // не долго думая отвечает клиенту
-                // выталкиваем все из буфера
                 if (Integer.parseInt(word) == (int) (Math.random() * 10)){
                     out.write("You win" + "\n");
                     out.flush();
